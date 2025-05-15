ef0ee0109a03626e7ae546de8645634a40a9944a
Onitiri Ibraheem <onit0001@algonquinlive.com>
2025-05-15 16:30:18 +0100

    Added script.js and an index file

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+// console.log will output a message to the developer console
+// this is a single line comment
+console.log("Hello World");
+
+/*
+this is a multi line comment
+it starts with a / and a * and ends with a * and a /
+*/
+
+//to declare a variable we use let or const
+const ourClassNum = "J206";
+let numStudents = 9; //no quotes needed for numbers
+
+console.log(ourClassNum);
+
+//if you declare a variable with let you can change it value
+numStudents = 21;
+
+console.log(numStudents);
+
+//if you declare a variable with const you cannot change it value
+//ourClassNum = "J207";
+//trying to change a const variable will cause an error
+
+console.log(ourClassNum);
+
+//const and let can not be redeclared (or recreated)
+//let numStudents = 25; (this will cause an error)
+
